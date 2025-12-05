da502e28ccf19703a0db113727026c2e38360ced
YuJeong <qordbwjd96@gmail.com>
2025-12-05 18:07:17 +0900

    [feat] Spring 프레임워크 고급 - 초기 설정 및 엔티티 구현

---
+++ 2025-12-05/yujeong/User.java
@@ -0,0 +1,26 @@
+package com.yujeong.board.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 200)
+    private String name;
+
+    @Column(nullable = false, length = 1000)
+    private String email;
+
+    @Column(nullable = false, length = 1000)
+    private String password;
+}
